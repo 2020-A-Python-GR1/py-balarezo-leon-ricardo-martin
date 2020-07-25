11d1db74a17d14d0bd3cba0e3a11f4a37c4ad1e0
Ricardo Leon <martinbalarezo92@hotmail.com>
2020-07-25 10:47:17 -0500

    clase Dataframes

--- 03 - Pandas/c_dataframes.py
+++ 03 - Pandas/c_dataframes.py
@@ -10,3 +10,79 @@ Created on Tue Jul 21 08:47:00 2020
 
 import numpy as np
 import pandas as pd
+
+arr_pand = np.random.randint(0,10,6).reshape(2,3)
+
+#crear el Dataframe
+df1 = pd.DataFrame(arr_pand)
+
+s1 = df1[0]
+
+s2 = df1[1]
+
+s3 = df1[2]
+
+#añadir valores al dataframe
+
+df1[3] = s1
+
+df1[4] = s1 * s2
+
+
+datos_fisicos_uno = pd.DataFrame(
+    arr_pand,
+    columns = [
+        'Estatura (cm)',
+        'Peso (kg)',
+        'Edad (anios)']
+    
+    )
+
+datos_fisicos_dos = pd.DataFrame(
+    arr_pand,
+    columns = [
+        'Estatura (cm)',
+        'Peso (kg)',
+        'Edad (anios)'],
+    index = [
+        'Martin',
+        'Ricardo'
+        ]
+    )
+
+
+
+
+#seleccionar una serie específica
+
+print("#################")
+
+serie_peso = datos_fisicos_dos['Peso (kg)']
+datos_martin = serie_peso['Martin']
+
+print(serie_peso)
+print(datos_martin)
+
+
+df1.index = ['Martin', 'Ricardo']
+df1.index = ['Emily', 'Francis']
+
+df1.columns = ['A','B','C','D','E']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 03 - Pandas/d_lectura_csv.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 25 10:07:40 2020
+
+@author: martinb1992
+"""
+
+import numpy as np
+import pandas as pd
+
+path = "C:/Users/marti/Documents/Phyton/Repositorios Git/repos/py-balarezo-leon-ricardo-martin/03 - Pandas/data/artwork_data.csv"
+
+df1 = pd.read_csv(
+    path,
+    nrows=10
+    )
+
+columnas = [
+    'id',
+    'artist',
+    'title',
+    'medium',
+    'year',
+    'acquisitionYear',
+    'height',
+    'width',
+    'units'
+    
+    ]
+
+df2 = pd.read_csv(
+    path,
+    nrows=10,
+    usecols = columnas,
+    index_col='id')
