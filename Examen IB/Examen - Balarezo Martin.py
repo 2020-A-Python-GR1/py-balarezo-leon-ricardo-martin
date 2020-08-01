a255733125069bebdd8fb7f62188e2c8ce21be9d
Ricardo Leon <martinbalarezo92@hotmail.com>
2020-08-01 09:10:32 -0500

    exmane

---
+++ Examen IB/Examen - Balarezo Martin.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  1 09:06:21 2020
+
+@author: martinb1992
+"""
+
+
+import numpy as np
+# 1) Examen
+
+## 2) Crear un vector de ceros de tamaño 10
+
+arreglo_de_ceros = np.arange(10);
+
+arreglo_de_ceros
+
+## 3) Crear un vector de ceros de tamaño 10 y el de la posicion 5 sea igual a 1
+
+## 4) Cambiar el orden de un vector de 50 elementos, el de la posicion 1 es el de la 50 etc.
+
+## 5) Crear una matriz de 3 x 3 con valores del cero al 8
+
+## 6) Encontrar los indices que no sean cero en un arreglo
